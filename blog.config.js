3c96db1f34e87bc5da1990d676904e540a475438
zqdongxw <zqdongxw@gmail.com>
2024-02-26 22:25:56 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '印心宗', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '海外印心精舍🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'hwyxjs.org', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'blog.customai.win', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '印心精舍, 心中心', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
