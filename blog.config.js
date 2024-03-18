798896b726b2a20908c1f6191cf76867a418aed4
zqdongxw <zqdongxw@gmail.com>
2024-03-18 09:26:35 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '印心宗', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'null', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'blog.customai.win', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'www.yinxin.pp.ua/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '印心精舍, 心中心', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
@@ -237,7 +237,7 @@ const BLOG = {
   DIFY_CHATBOT_BASE_URL: process.env.NEXT_PUBLIC_DIFY_CHATBOT_BASE_URL || '',
   DIFY_CHATBOT_TOKEN: process.env.NEXT_PUBLIC_DIFY_CHATBOT_TOKEN || '',
   // 悬浮挂件
-  WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
+  WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || false, // 是否显示宠物挂件
   WIDGET_PET_LINK:
         process.env.NEXT_PUBLIC_WIDGET_PET_LINK ||
         'https://cdn.jsdelivr.net/npm/live2d-widget-model-wanko@1.0.5/assets/wanko.model.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
